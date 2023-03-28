6f32a03691a5512dc8e0a86b33ee566841739bf9
blusman <1622323681@qq.com>
2023-03-28 14:17:15 +0800

    路径调整

--- js/03-data.js
+++ js/03-data.js
@@ -29,7 +29,7 @@ var data = [
     }
 ]
 
-var data1 = ['file:///E:/%E5%89%91%E5%BF%83/Desktop/%E7%AE%80%E5%8D%95%E7%89%B9%E6%95%88/html/01-hot-heart.html', 'file:///E:/%E5%89%91%E5%BF%83/Desktop/%E7%AE%80%E5%8D%95%E7%89%B9%E6%95%88/html/02-run-images.html', 'file:///E:/%E5%89%91%E5%BF%83/Desktop/%E7%AE%80%E5%8D%95%E7%89%B9%E6%95%88/html/04-post-msg.html']
+var data1 = ['https://blusman.github.io/my-demo/html/01-hot-heart.html', 'https://blusman.github.io/my-demo/html/02-run-images.html', 'https://blusman.github.io/my-demo/html/04-post-msg.html']
 var btn = document.querySelector('button');
 var text = document.querySelector('input');
 var main = document.querySelector('.main');
